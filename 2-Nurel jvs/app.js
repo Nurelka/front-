fe464f81833799bd0679ef2538bfb06f5b2034ab
Nurelka <ryskulbekoanurel@gmail.com>
2024-05-13 10:42:48 +0600

    first commit

---
+++ 2-Nurel jvs/app.js
@@ -0,0 +1,17 @@
+const age=prompt('how  old are you')
+   (true && true)=true
+   (true && false)=false
+if(age<13 && age>0){
+    console.log('Wellcome');
+} else if (age<18 && age>14){
+    console.log('oilonup korolu');
+} else{
+    console.log('Sorry');
+}
+(true && true)=true
+(true && false)=false
+(false && false)=false
+
+(true || true)=true
+(false|| true)=true
+(false|| false)=false 
---
+++ app.js
@@ -0,0 +1,52 @@
+// const myName="Ilim"
+// console.log(myName);
+// let mycoAge="25"
+// console.log(myAge);
+
+// myAge="29"
+// console.log(myAge);
+// myAge="14"
+// console.log(myAge);
+
+// letAge='25'
+// let message='Hello '+myName+' '+myAge+ "Welcom to JS"
+// let newMessage=`Hello${myName} ${myAge} Welcome to JS`
+// console.log(message); 
+// console.log(newMessage);
+
+// const userName=prompt('What is your name')
+// const isMarriesd=confirm('Are you  merried')
+// alert(userName)
+
+
+// constnum=parseInt(prompt('first san jaz'));
+// const num2=parseInt(prompt ('second number'))
+// const summa=num+num2
+// const. log (summa);
+// const minus=num-num2;
+// console.log (num+'-'+num2+'='+ minus);
+// const umnoj=num*num2
+// console.log(umnoj); 
+
+ const userName=prompt('What is your name?')
+ console.log(userName);
+ const useraddress=prompt('Are you addres')
+ console.log(useraddress);
+ const userhouse=prompt('Where do you live')
+ console.log(userhouse);
+ const userhowoldareyou=prompt('How old are you ')
+ console.log(userhowoldareyou);
+ const userparents=prompt('What are the parents name')
+ console.log(userparents);
+ aler=(userName)
+
+
+
+ const num = parseInt(prompt("first san"));
+ const num2 = parseInt(prompt("second number"));
+// const summa = num+num2;
+//  console.log(summa);
+const minus=num-num2
+ console.log(num+'-'+num2+'='+'minus');
+ const umnoj=num/num2
+ console.log(umnoj);
